6ca91e94546d9c14c1e2eb3e072f6d2d5378371a
Aquiles Alves <aquilespereira789@gmail.com>
2025-05-13 15:49:42 -0300

    Update CalculadoraProporcional.py

--- CalculadoraProporcional.py
+++ CalculadoraProporcional.py
@@ -1,6 +1,12 @@
-valor_plano = float(input("Digite o valor atual do plano: "))
-dias_acrescentados = int(input("Digite os dias acrescentados no plano: "))
+
+vencimento_atual = int(input('Digite o vencimento atual: '))
+vencimento_novo = int(input('Digite o novo vencimento: '))
+valor_plano = float(input('Digite o valor atual do plano: '))
+
+dias_proporcionais = vencimento_novo - vencimento_atual
+
+
 diaria_plano = valor_plano / 30
-proporcional = diaria_plano * dias_acrescentados
+proporcional = diaria_plano * dias_proporcionais
 valor_total = proporcional + valor_plano
 print(valor_total)
