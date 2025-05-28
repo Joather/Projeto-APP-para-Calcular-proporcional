1dc312cc561fe360bbec92f00656e944ddb2a820
Joaquim Vitor <135838552+Joather@users.noreply.github.com>
2025-05-27 21:27:10 -0300

    Update CalculadoradeProporcional.py

--- Calculadora de Proporcional/CalculadoradeProporcional.py
+++ Calculadora de Proporcional/CalculadoradeProporcional.py
@@ -5,7 +5,7 @@ from tkinter import messagebox
 
 # Configuração inicial
 ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("blue")
+ctk.set_default_color_theme("green")
 
 app = ctk.CTk()
 app.title("Calculadora de Proporcional")
